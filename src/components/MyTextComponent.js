83f9bc8ba1026b1c7af6105217ab5e48301ca59b
VaibhavMojidra <vmojidra25@gmail.com>
2021-03-03 15:42:43 +0530

    Adding

---
+++ src/components/MyTextComponent.js
@@ -0,0 +1,8 @@
+import React from 'react'; // Import react
+import { Text } from 'react-native'; //Import only specific
+
+const MyTextComponent=()=>{
+    return(<Text>Hello World</Text>);
+};
+
+export default MyTextComponent;
